87ab086226ffa96946eec1c9c765202835ba9dc9
yukihane <yukihane.feather@gmail.com>
2019-07-05 23:53:04 +0900

    update configuration

--- service-provider/src/main/java/com/example/AuthorizationServerConfiguration.java
+++ service-provider/src/main/java/com/example/AuthorizationServerConfiguration.java
@@ -1,9 +1,29 @@
 package com.example;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableAuthorizationServer
 @Configuration
-public class AuthorizationServerConfiguration {
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+            .inMemory()
+            .withClient("client")
+            .secret(passwordEncoder().encode("secret"))
+            .scopes("read", "write")
+            .authorizedGrantTypes("authorization_code", "password")
+            .redirectUris("http://localhost:18080/client/tasks");
+    }
 }
---
+++ service-provider/src/main/java/com/example/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+            User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build());
+    }
+}
